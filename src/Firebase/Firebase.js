c524938a6c92598f4a3a60c7f0ab07fd10e2ebd7
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2023-04-15 11:16:14 -0700

    fix leaked api key

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,24 +1,25 @@
 // Import the functions you need from the SDKs you need
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD3E95v5r0XAC2KP72T5lg76H-mqNOjXoQ",
-  authDomain: "acm-hacks.firebaseapp.com",
-  projectId: "acm-hacks",
-  storageBucket: "acm-hacks.appspot.com",
-  messagingSenderId: "933115887831",
-  appId: "1:933115887831:web:99a97133ffb6b19e0f3092",
-  measurementId: "G-59P79X8PCH"
+const FirebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp( firebaseConfig );
+const app = initializeApp( FirebaseConfig );
 const analytics = getAnalytics( app );
 
 export const auth = getAuth( app );
