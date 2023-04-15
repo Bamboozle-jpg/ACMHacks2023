a2af76eecb7aec134521a351bbdb4c1884790d4d
ASadDorito <58340393+ASadDorito@users.noreply.github.com>
2023-04-15 07:59:27 -0700

    landing page prettifying, may be some margin issues

--- src/pages/LandingScreen.jsx
+++ src/pages/LandingScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 
 import { useNavigate } from 'react-router-dom'
 
@@ -6,9 +7,14 @@ function LandingScreen() {
   const nav = useNavigate();
 
   return (
-    <div>
-    LandingScreen <br></br>
-    <button onClick={ () => nav( "/1" ) }>Sign In</button>
+    <div id="background">
+        <h1 id='title'>LandingScreen</h1>
+        <br></br>
+    
+        {/*These three are all the same redirect*/}
+        <button onClick={ () => nav( "/1" ) }id='button'>Sign Up</button>  <br></br><br></br>
+        <button onClick={ () => nav( "/1" ) }id='button'>Log in</button>  <br></br><br></br>
+        <button onClick={ () => nav( "/1" ) }id='button'>Go to Site </button>  <br></br><br></br>
     </div>
   )
 }
