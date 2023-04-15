dd910ddfbea78d549a5cb62e8b42b479e1066a55
Bamboozle-jpg <kevin@bachelorclan.com>
2023-04-15 11:42:49 -0700

    side panel finished

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route } from 'react-router-dom';
 
 import TestPage1 from './pages/TestPage1.jsx';
 import TestPage2 from './pages/TestPage2.jsx';
+import SideBar from './pages/SideBar.jsx';
 
 import Test from './pages/Test';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/1" element={ <TestPage1/>} />
         <Route path="/2" element={ <TestPage2/>} />
         <Route path="/4" element={ <Test/> } />
+        <Route path="/5" element={ <SideBar/> } />
       </Routes>
     </div>
   );
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,20 +3,30 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function MainPage() {
-  const nav = useNavigate();
+    const nav = useNavigate();
 
-  return (
-    <div>
-      <h1>
-        MainPage Yay!!
-        {/* BEGIN YOUR MAINPAGE EDITING HERE */}
-      </h1>
+    return (
+        <div>
+            <div class="wrapper">
+            <div class="sidePanelBody">
+                
+            </div>
+            <button class="sidePanelToggle" type="button">
+                <span class="openIcon">IMAGE HERE</span>
+                <span class="closeIcon">Imaage Here</span>
+                
+            </button>
+            </div>
+            <h1>
+                MainPage Yay!!
+                {/* BEGIN YOUR MAINPAGE EDITING HERE */}
+            </h1>
 
-      <button onClick={ () => nav( "1" ) }>Page 1</button>
-      <button onClick={ () => nav( "2" ) }>Page 2</button>
-      <button onClick={ () => nav( "/4" ) }>Test</button>
-    </div>
-  )
+            <button onClick={ () => nav( "1" ) }>Page 1</button>
+            <button onClick={ () => nav( "2" ) }>Page 2</button>
+            <button onClick={ () => nav( "/4" ) }>Test</button>
+        </div>
+    )
 }
 
 export default MainPage
