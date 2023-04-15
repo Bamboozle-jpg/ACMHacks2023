9d1418b73e470eb9ed13ab0e5b8b7276a531815c
ASadDorito <58340393+ASadDorito@users.noreply.github.com>
2023-04-15 09:47:41 -0700

    created NameSearch and corresponding CSS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Route } from 'react-router-dom';
 
 import TestPage1 from './pages/TestPage1.jsx';
 import TestPage2 from './pages/TestPage2.jsx';
-
+import NameSearch from './pages/NameSearch';
 import MainPage from './pages/MainPage.jsx';
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={ <MainPage/>} />
         <Route path="/1" element={ <TestPage1/>} />
         <Route path="/2" element={ <TestPage2/>} />
+        <Route path="/6" element={ <NameSearch/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/NameSearch.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../App.css'
+import { useNavigate } from 'react-router-dom'
+
+
+function NameSearch() {
+  const nav = useNavigate();
+  return (
+    <div id='background'>
+        <h1 id='title'>NameSearch</h1>
+        <NameButton />
+        <h3 id='generatebar'>hello</h3>
+    </div>
+
+  )
+}
+function NameButton() {
+    var nameList = ['tom', 'penis', 'harry', 'larry', 'barry']
+    function writeName() {
+        const random = Math.floor(Math.random() * nameList.length);
+        document.getElementById("generatebar").textContent = nameList[random];
+    }
+  
+    return (
+      <button onClick={writeName}id='eventbutton'>
+        Clicked this to get a name!
+      </button>
+    );
+  }
+
+export default NameSearch
