01f4b931bf2433d2282231b97be2fe14ac47a05c
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2023-04-15 09:33:07 -0700

    removed comment

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -22,38 +22,6 @@ function MainPage() {
   const [ Name, SetName ] = useState( "None" );
 
 
-  // const HandleSignIn = ( e ) => {
-  //   e.preventDefault();
-  //   console.log( 'we made it here' );
-  //   // signInWithRedirect( auth, provider );
-
-  //   signInWithRedirect( auth, provider )
-  //   .then((result) => {
-  //     // This gives you a Google Access Token. You can use it to access the Google API.
-  //     const credential = GoogleAuthProvider.credentialFromResult(result);
-  //     const token = credential.accessToken;
-  //     // The signed-in user info.
-  //     const user = result.user;
-  //     // IdP data available using getAdditionalUserInfo(result)
-  //     // ...
-
-  //     SetName( user );
-  //     console.log( user );
-  //     console.log( 'logged in' );
-
-  //   }).catch((error) => {
-  //     // Handle Errors here.
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     // The email of the user's account used.
-  //     const email = error.customData.email;
-  //     // The AuthCredential type that was used.
-  //     const credential = GoogleAuthProvider.credentialFromError(error);
-  //     // ...
-  //   });
-  // }
-
-
   const HandleSignIn = async ( e ) => {
     try {
       await GoogleLogIn();
@@ -71,6 +39,7 @@ function MainPage() {
 
     console.log( "Signed out" );
   }
+
   
   return (
     <div>
