8e19e38f34d7f980d89358f0cce1ec6759f61318
ASadDorito <58340393+ASadDorito@users.noreply.github.com>
2023-04-15 12:22:08 -0700

    randomized name generator and stuff

--- src/pages/NameSearch.jsx
+++ src/pages/NameSearch.jsx
@@ -10,15 +10,32 @@ function NameSearch() {
         <h1 id='title'>NameSearch</h1>
         <NameButton />
         <h3 id='generatebar'>hello</h3>
+        <h3 id='timer'>bruh</h3>
     </div>
 
   )
 }
+
+/*var delay = 8000;
+var lastClick = 0;
+var interval = setInterval(NameButton, 5000);
+*/
+
 function NameButton() {
     var nameList = ['tom', 'penis', 'harry', 'larry', 'barry']
     function writeName() {
+        /*
+        console.log(lastClick);
+
+        if (lastClick >= (Date.now() - delay)){
+            document.getElementById("timer").textContent = delay/1000 + " seconds";
+            return;
+        }
+        lastClick = Date.now();
+        */
         const random = Math.floor(Math.random() * nameList.length);
         document.getElementById("generatebar").textContent = nameList[random];
+
     }
   
     return (
