b3b54bf71d2da1ffc52fbffb3d9869b6a45b5b15
ASadDorito <58340393+ASadDorito@users.noreply.github.com>
2023-04-15 22:48:04 -0700

    small bugfixes with duplicated code in App.js

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ import TestPage1 from './pages/TestPage1';
 import TestPage2 from './pages/TestPage2.jsx';
 import NameSearch from './pages/NameSearch';
 
-function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={ <MainPage/>} />
-        <Route path="/1" element={ <TestPage1/>} />
-        <Route path="/2" element={ <TestPage2/>} />
-        <Route path="/6" element={ <NameSearch/>} />
-      </Routes>
-    </div>
 import SideBar from './pages/SideBar.jsx';
 import Chat from './pages/Chat.jsx';
 
@@ -44,6 +34,7 @@ function App() {
           <Route path="/4" element={ <Chat/> } />
           <Route path='/5' element={ <UserInfo/>} />
           <Route path='/6' element={ <ChatBar/>} />
+          <Route path="/7" element={ <NameSearch/>} />
           <Route
             path="/chatroom"
             element={ 
